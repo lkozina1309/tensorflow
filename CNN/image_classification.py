5979dc8ceb40756d30850e94069b94be0467bda5
SAT <65762882+lkozina1309@users.noreply.github.com>
2021-11-22 14:58:31 +0100

    Update image_classification.py

--- CNN/image_classification.py
+++ CNN/image_classification.py
@@ -4,12 +4,13 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers, datasets, models
 
+
 img_height = 180
 img_width = 180
 batch_size = 32
 
 ds_train = tf.keras.utils.image_dataset_from_directory(
-	'data/vehicles', #path
+	'/data/vehicles', #path
 	batch_size=batch_size,
 	image_size = (img_height, img_width), 
 	shuffle=True,
@@ -48,13 +49,17 @@ num_classes = 5
 
 model = tf.keras.Sequential([
 	layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+	tf.keras.layers.Conv2D(8, 3, activation='relu'),
+	tf.keras.layers.MaxPooling2D((2,2)),
+	tf.keras.layers.Conv2D(16, 3, activation='relu'),
+	tf.keras.layers.MaxPooling2D((2,2)),
 	tf.keras.layers.Conv2D(32, 3, activation='relu'),
-	tf.keras.layers.MaxPooling2D(),
+	tf.keras.layers.MaxPooling2D((2,2)),
 	tf.keras.layers.Conv2D(64, 3, activation='relu'),
-	tf.keras.layers.MaxPooling2D(),
-	tf.keras.layers.Conv2D(128, 3, activation='relu'),
+	tf.keras.layers.MaxPooling2D((2,2)),
+	tf.keras.layers.Conv2D(64, 3, activation='relu'),
 	tf.keras.layers.Flatten(),
-	tf.keras.layers.Dense(128, activation='relu'),
+	tf.keras.layers.Dense(64, activation='relu'),
 	tf.keras.layers.Dense(num_classes),
 ])
 		
@@ -64,8 +69,7 @@ model. compile(
 	metrics=["accuracy"],
 )
 
-model.fit(ds_train, validation_data=ds_validation, epochs=4)
+model.fit(ds_train, validation_data=ds_validation, epochs=15)
 
 loss, acc = model.evaluate(ds_validation)
 print("Accuracy", acc)
-
