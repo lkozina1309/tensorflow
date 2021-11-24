e2de110c2baa9813405321cb5a15b010f7b0d2a1
SAT <65762882+lkozina1309@users.noreply.github.com>
2021-11-24 16:30:12 +0100

    Update video_face_detection.py

--- CNN/MTCNN/video_face_detection.py
+++ CNN/MTCNN/video_face_detection.py
@@ -1,17 +1,18 @@
 import cv2
 from mtcnn.mtcnn import MTCNN
 
-detector = MTCNN()
 cap = cv2.VideoCapture('Megamind.avi')
 
 while cap.isOpened():
 	_, img = cap.read()
+	img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+	detector = MTCNN()
 	faces = detector.detect_faces(img)
 	
-	for result in faces:
-		x, y, w, h = result['box']
-		x1, y1 = x + w, y + h
-		cv2.rectangle(img, (x, y), (x1, y1), (0, 0, 255), 2)
+	for i in range (len(faces)):
+		x1, y1, width, height = faces[i]['box']
+		x2, y2 = x1 + width, y1 + height
+		cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
 		
 	cv2.imshow('img', img)
 	if cv2.waitKey(1) & 0xFF == ord('q'):
